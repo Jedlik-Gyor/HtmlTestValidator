5b78db616c485c9f1a5b49a8b8099acc5f0e9c7a
Bognár Pál <bognar.pal@outlook.hu>
2022-04-25 13:22:23 +0200

    Reogranize projects

---
+++ HtmlTestValidator.Console/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Linq;
+using HtmlTestValidator.Models;
+using HtmlTestValidator.Models.Project;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace HtmlTestValidator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length >= 2) {
+                var taskJsonPath = args[0];
+                var testParentFolderPath = args[1];
+
+                EvaluationSheet evaluationSheet = null;
+                if (!File.Exists(taskJsonPath))
+                {
+                    Console.WriteLine("A definicós állomány a megadott elérési úton nem létezik");
+                    return;
+                }
+                if (!Directory.Exists(testParentFolderPath))
+                {
+                    Console.WriteLine("A beadott dolgozatokhoz megadott könyvtár nem létezik");
+                    return;
+                }
+                Project project;
+                try
+                {
+                    project = Project.ReadFromFile(taskJsonPath);
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine($"Json beolvaási hiba: {ex.Message}");
+                    return;
+                }
+                var evaluations = Directory.GetDirectories(testParentFolderPath)
+                                        .Select(d => new Evaluation(d, project.Steps.Length))
+                                        .ToList();
+
+                Parallel.ForEach(evaluations,
+                                new ParallelOptions { MaxDegreeOfParallelism = 5 },
+                                evaluation => evaluation.Evaluate(project));
+
+                evaluationSheet = new EvaluationSheet(project, evaluations);
+                evaluationSheet.SaveAs("teszt.xlsx");
+
+                evaluationSheet.Dispose();
+                Process.Start(new ProcessStartInfo("teszt.xlsx") { UseShellExecute = true });
+            }
+        }
+
+        
+    }
+}
