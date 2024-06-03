c27aaf544ca9a9f0eacb9c345b5b13fe7164e7ef
Bognár Pál <bognar.pal@outlook.hu>
2024-06-03 23:16:18 +0200

    paraméterezés

--- HtmlTestValidator.WPF/App.xaml.cs
+++ HtmlTestValidator.WPF/App.xaml.cs
@@ -19,9 +19,9 @@ namespace HtmlTestValidator
             var taskJsonPath = "";
             var testParentFolderPath = "";
             if (e.Args.Length >= 1)
-                taskJsonPath = e.Args[0];
+                taskJsonPath = e.Args[0].Trim();
             if (e.Args.Length >= 2)
-                testParentFolderPath = e.Args[1];
+                testParentFolderPath = e.Args[1].Trim();
             MainWindow wnd = new MainWindow(taskJsonPath, testParentFolderPath);
             wnd.Show();
             
